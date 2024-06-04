f4d309886f6d00fc232a370a939b1b4c2ae8cb54
Jorge García Jiménez <jorge.garcia@liferay.com>
2024-06-04 15:52:38 +0200

    LPD-1240 send contextPath based on themeDisplay.getPathContext and fallback to slash if none is configured

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/utilities/cookies.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/utilities/cookies.js
@@ -19,7 +19,7 @@ class CommerceCookie {
 		return getCookie(this.scope + key, this.cookieType);
 	}
 
-	setValue(key, value, expires, path = '/') {
+	setValue(key, value, expires, path = themeDisplay.getPathContext() || '/') {
 		const options = {path};
 
 		if (expires) {
