3f39054df09b9a8182c8de4cab9b049e60e60928
Jorge García Jiménez <jorge.garcia@liferay.com>
2024-07-31 16:58:05 +0200

    LPD-32888 SF

--- modules/test/playwright/tests/login-web/forgotPasswordCaptcha.spec.ts
+++ modules/test/playwright/tests/login-web/forgotPasswordCaptcha.spec.ts
@@ -5,16 +5,25 @@
 
 import {expect, test} from '@playwright/test';
 
-import {liferayConfig} from "../../liferay.config";
+import {liferayConfig} from '../../liferay.config';
 
-test('LPD-32888 Check captcha verification text is cleared after a wrong captcha',
-    async ({page}) => {
-        await page.goto(liferayConfig.environment.baseUrl);
-        await page.getByRole('button', {name: 'Sign In'}).click();
-        await page.getByText('Forgot Password').click();
-        await page.waitForTimeout(1000);
-        await page.getByLabel('Email Address').fill('test@liferay.com');
-        await page.locator('[id="_com_liferay_login_web_portlet_LoginPortlet_captchaText"]').fill('abcd');
-        await page.getByRole('button', {name: 'Send new password'}).click();
-        await expect(page.locator('[id="_com_liferay_login_web_portlet_LoginPortlet_captchaText"]')).toBeEmpty();
+test('LPD-32888 Check captcha verification text is cleared after a wrong captcha', async ({
+	page,
+}) => {
+	await page.goto(liferayConfig.environment.baseUrl);
+	await page.getByRole('button', {name: 'Sign In'}).click();
+	await page.getByText('Forgot Password').click();
+	await page.waitForTimeout(1000);
+	await page.getByLabel('Email Address').fill('test@liferay.com');
+	await page
+		.locator(
+			'[id="_com_liferay_login_web_portlet_LoginPortlet_captchaText"]'
+		)
+		.fill('abcd');
+	await page.getByRole('button', {name: 'Send new password'}).click();
+	await expect(
+		page.locator(
+			'[id="_com_liferay_login_web_portlet_LoginPortlet_captchaText"]'
+		)
+	).toBeEmpty();
 });
